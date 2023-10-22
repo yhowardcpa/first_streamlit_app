7e274274aed63115ce2f64cc72e5150e844f7150
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 16:12:10 -0500

    Update streamlit_app.py
    i have no idea why the response is not translating over to stream lit

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,7 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text = fruityvice_response
-
+streamlit.text(fruityvice_response)
 
    
 
