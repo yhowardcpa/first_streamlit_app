991ea7d9249b779c58661cd2d53336d83f5774a1
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 10:26:09 -0500

    Update streamlit_app.py
    I wanted to see what would happen if the image was added in the middle of the text for the chicken and nothing happened...OMG...awesomeness.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 OKale, Spinach & Rocket Smoothie')
-streamlit.text('Hard🐔 -Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacodo Toast')
    
 
