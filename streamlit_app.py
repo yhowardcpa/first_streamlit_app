40ab6252e851ebe0bce6125ba5519af888a1d734
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 16:13:13 -0500

    Update streamlit_app.py
    still getting an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 import pandas
 
+import requests
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list =my_fruit_list.set_index('Fruit')
 
@@ -32,7 +34,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display FruityVice API response
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
