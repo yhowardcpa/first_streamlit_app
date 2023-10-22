8a446f0bf710222abbe822eac337cf5211b4f24c
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 11:17:24 -0500

    Update streamlit_app.py
    i have no idea why there is an error, this time i will try to see what is causing it before i start the editing process the next time.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Let's put a pick list here so they can pick the fruit they want to include
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 
 #Display the fruit list  on the page
