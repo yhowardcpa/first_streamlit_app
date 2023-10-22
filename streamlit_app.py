073ca6af02806d7bcdb7748d53b15390e6c004b0
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 10:24:16 -0500

    Update streamlit_app.py
    Added pictures...wow that is done easily by just including it in the single quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('OKale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 OKale, Spinach & Rocket Smoothie')
+streamlit.text('Hard🐔 -Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacodo Toast')
+   
 
