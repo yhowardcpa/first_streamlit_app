05d094a434314acb55dd17c341128cf4dd9be947
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 16:35:37 -0500

    Update streamlit_app.py
    This is so troubling i have no idea why this is no longer working

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,12 @@ my_fruit_list =my_fruit_list.set_index('Fruit')
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast what happened')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 OKale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+#🥣 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
