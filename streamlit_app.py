15ba90d0828c08d88552a5554b27d2a5af451644
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 10:19:28 -0500

    Update streamlit_app.py
    Added a header with Breakfast menu and changed from .write to .text for the menu items as described in the Snowflake training.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('OKale, Spinach & Rocket Smoothie')
-streamlit.write('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('OKale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
