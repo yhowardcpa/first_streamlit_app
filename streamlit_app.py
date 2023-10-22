7dfe7ab6c5c49768a73a4badacaa3e1fc648c945
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 10:58:30 -0500

    Update streamlit_app.py
    We are changing the selection drop down to include the name of fruit instead of the index.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list =my_fruit_list.set_index('Fruit')
+
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
