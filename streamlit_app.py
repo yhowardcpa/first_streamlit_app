508edf2fc034cb48d19c0ec4f1f3985a713be502
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 10:14:02 -0500

    Update streamlit_app.py
    Now that I know that .write is for plain text...i added the other menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.write('Omega 3 & Blueberry Oatmeal')
+streamlit.write('OKale, Spinach & Rocket Smoothie')
+streamlit.write('Hard-Boiled Free-Range Egg')
+
