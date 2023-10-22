1ab143e40100765ba6104835178b7c5f5bae8a8d
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-22 10:38:07 -0500

    Update streamlit_app.py
    Addition of import of pandas library and the use of a csv file to add to the menu is my guess.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+
+import pandas
+
+myfruitlist = pandas.read.csv(https://uni-lab-files.s3.us-east-2.amazonaws.com/dabw/fruit_macros.txt)
+
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -7,6 +12,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacodo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
 
    
 
