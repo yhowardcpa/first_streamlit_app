496ce66d97171e55e622155efdbabf65cc4016b1
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 16:56:15 -0500

    Update streamlit_app.py
    updated to accept user choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 streamlit.text(fruityvice_response.json())  #just writes the value to the screen
 
 # Normalizes the json response from above
