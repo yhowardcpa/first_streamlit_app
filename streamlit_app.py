7401f745e1f98bc8b181369940142e1a7cf38f95
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 16:52:13 -0500

    Update streamlit_app.py
    customer makes the selection or provides input

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,11 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display FruityVice API response
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 streamlit.text(fruityvice_response.json())  #just writes the value to the screen
