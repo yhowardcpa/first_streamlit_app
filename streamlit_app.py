6997bb1da4555838997478c6bbd4bfb67f622806
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 15:08:07 -0500

    Update streamlit_app.py
    comment out line 11

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import snowflake.connector
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#streamlit.text(my_data_row)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list =my_fruit_list.set_index('Fruit')
