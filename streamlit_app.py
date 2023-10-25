58df4717e3dbc1076df17550af6cf559cb3d431e
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 17:26:35 -0500

    Update streamlit_app.py
    moved to the bottom of the webpage

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list =my_fruit_list.set_index('Fruit')
@@ -55,6 +49,14 @@ streamlit.text(fruityvice_response.json())  #just writes the value to the screen
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # The normalized data is now placed in a table
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
    
 
