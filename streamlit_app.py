4698fc492d344381f55ef9ccbad33d8ac2ff231c
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 16:50:12 -0500

    Update streamlit_app.py
    added all fruit plus kiwi?

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 streamlit.text(fruityvice_response.json())  #just writes the value to the screen
 
 # Normalizes the json response from above
