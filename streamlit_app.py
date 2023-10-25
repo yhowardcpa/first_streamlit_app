f2e26ea5c9349c762c9956deb75ca9aaabf72a0a
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 18:25:37 -0500

    Update streamlit_app.py
    missing colon..

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ streamlit.header("Fruityvice Fruit Advice")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
    streamlit.write('The user entered ', fruit_choice)
-   if not fruit_choice
+   if not fruit_choice:
    streamlit.error("Please select a fruit to get information.")
    else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
