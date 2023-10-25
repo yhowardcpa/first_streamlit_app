32a4b77982b33ff3a2b95c6d69e9fa03830b5fdd
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 15:02:50 -0500

    Update streamlit_app.py
    correct error in snowflake connector.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake-connector
+import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
