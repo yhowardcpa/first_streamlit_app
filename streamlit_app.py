cfaa0ce5edd60ccf2529b10f6907f72be01a4d45
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 18:23:14 -0500

    Update streamlit_app.py
    adding indentions

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,9 @@ streamlit.header("Fruityvice Fruit Advice")
 try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-if not fruit_choice
-streamlit.error("Please select a fruit to get information.")
-else:
+   if not fruit_choice
+   streamlit.error("Please select a fruit to get information.")
+   else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 streamlit.text(fruityvice_response.json())  #just writes the value to the screen
 
