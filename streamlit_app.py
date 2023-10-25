31ffb8f724383c547ebff1834ffac6790571037a
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 17:24:14 -0500

    Update streamlit_app.py
    removed comments for the new code...let's see I know how to troubleshoot now.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ import pandas
 import requests
 import snowflake.connector
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
+streamlit.text(my_data_row)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list =my_fruit_list.set_index('Fruit')
