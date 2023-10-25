9f08ba69b85dca9806d9eb2c4fd90a7f45eb240d
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-10-25 18:24:36 -0500

    Update streamlit_app.py
    more indentions

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   streamlit.write('The user entered ', fruit_choice)
    if not fruit_choice
    streamlit.error("Please select a fruit to get information.")
    else:
